a18fc4c51621270f561fb1a744ed3ecff4fc1a03
dougiefreshxd <douglascerrato2@gmail.com>
2022-11-22 20:08:24 -0800

    More Planning and Testing
    Added some changes to the code, as well as started the modules within our class that we will use to figure out the fastest distance. Also added an outside global function used to compare routes to determine the best one. All still works in progress

--- pizza.py
+++ pizza.py
@@ -8,7 +8,7 @@ class Rules:
         '2M5L' : {'Medium' : 2, 'Large' : 5},
         '3M4L' : {'Medium' : 3, 'Large' : 4},
         '4M3L' : {'Medium' : 4, 'Large' : 3}
-        }
+    }
     #Reference image for connections should be included in the png in the folder holding this code
     #This is basically a python dictionary of a matric of the map we are working and the connections between the houses. We have the value for 
     #each key for a certain reason. We will make our trail for the fastest route by traversing each dictionary. For example since we start at 
@@ -22,13 +22,36 @@ class Rules:
         'C' : {'A' : 3, 'B' : 10, 'D' : 7},
         'D' : {'A' : 11, 'B' : 4, 'C' : 7, 'Z' : 5},
         'Z' : {'D' : 5}
-        }
-
+    }
+    #The point of this module is to take in 2 parameters, the starting point and ending point with the starting point being set to a default variable Z, which will represent
+    #our pizza shop that we will always start from. After that we declare a new dictionary to our Distances dictionary list, as we might want to edit this without editing
+    #the global dictionary. From here we are going to start a chain path, where we follow up the dictionary. Starting at Z we see what paths that has, which only is D with 5 miles
+    #From here, we note that number then look at the D key within our dictionary. Here we will see a new nested dictionary with multiple keys of houses we have routes too, and their 
+    #travel distance. We will later follow all these possible routes up to our ending point, and keep replacing the array/list with the fasted most updated route. Code still
+    #in progress
+    def fastest_route(self, ending_point, starting_point='Z'):
+        newdict =  self.Distances
+        distance_traveled = 0
+        #This is where the route and distance will be calculated
+        start = newdict[starting_point]
+        for x in start.keys():
+            distance_traveled += start[x]
+#What this function will do is help calculate the fastest route. When running our fastest route module within our earlier mentioned class, we will be saving the results of the 
+#first fastest route in an array, with the total distance being the first variable, then after that the list will have a trail of the starting point to the ending point
+#Then we will pass this array as a default variable within our comparing lists function below. And whenever we find another trail while going through all paths, we will compare
+#each array with the variables to the current_best route, and will only replace the default variable for the array if we find a faster quicker route
+def comparing_lists(current_best, contender):
+    return 0 
 #This is a temporary dictionary we will use to practice sending information into our algorithm and functions later to test capability and
 #debugging
-'''
-orders = {
-    
-}
-'''
+r1 = Rules()
 
+orders = {
+    'A' : {'Medium' : 2, 'Large' : 3},
+    'B' : {'Medium' : 0, 'Large' : 4},
+    'C' : {'Medium' : 2, 'Large' : 3},
+    'D' : {'Medium' : 4, 'Large' : 1}
+}
+
+orderlist = []
+r1.fastest_route(orderlist, 'A')
